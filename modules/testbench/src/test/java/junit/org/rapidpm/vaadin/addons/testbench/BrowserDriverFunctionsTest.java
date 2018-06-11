df6d95243ed140e83ffceb213fd9d6f828b604cc
dve <dve@vergien.net>
2018-06-11 15:41:05 +0200

    use correct port in test

--- modules/testbench/src/test/java/junit/org/rapidpm/vaadin/addons/testbench/BrowserDriverFunctionsTest.java
+++ modules/testbench/src/test/java/junit/org/rapidpm/vaadin/addons/testbench/BrowserDriverFunctionsTest.java
@@ -15,7 +15,7 @@ public class BrowserDriverFunctionsTest {
         .apply(BrowserDriverFunctions.CONFIG_FOLDER + "config").get();
 
     boolean isLocaleOrRapidSelnoid = "locale".equals(properties.get("unittesting.target"))
-        || "selenoid.rapidpm.org:80".equals(properties.get("unittesting.target"));
+        || "selenoid.rapidpm.org".equals(properties.get("unittesting.target"));
     assertTrue(isLocaleOrRapidSelnoid);
   }
 }
