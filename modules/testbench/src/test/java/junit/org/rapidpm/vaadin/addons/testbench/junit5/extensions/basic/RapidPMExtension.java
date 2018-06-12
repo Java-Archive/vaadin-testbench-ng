290b438f9ceb02bb0f496e86528957ed6a3eabfe
dve <dve@vergien.net>
2018-06-12 18:05:10 +0200

    Set correct ip

--- modules/testbench/src/test/java/junit/org/rapidpm/vaadin/addons/testbench/junit5/extensions/basic/RapidPMExtension.java
+++ modules/testbench/src/test/java/junit/org/rapidpm/vaadin/addons/testbench/junit5/extensions/basic/RapidPMExtension.java
@@ -1,9 +1,14 @@
 package junit.org.rapidpm.vaadin.addons.testbench.junit5.extensions.basic;
 
 
-import org.junit.jupiter.api.extension.*;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.rapidpm.dependencies.core.logger.HasLogger;
 import org.rapidpm.microservice.MainUndertow;
+import org.rapidpm.vaadin.addons.testbench.junit5.extensions.container.NetworkFunctions;
 import org.rapidpm.vaadin.addons.testbench.junit5.pageobject.PageObject;
 
 public class RapidPMExtension implements BeforeAllCallback, BeforeEachCallback,
@@ -30,7 +35,7 @@ public class RapidPMExtension implements BeforeAllCallback, BeforeEachCallback,
 
     System.setProperty(PageObject.SERVER_WEBAPP, MainUndertow.MYAPP);
     System.setProperty(PageObject.SERVER_PORT, MainUndertow.DEFAULT_SERVLET_PORT+"");
-
+    System.setProperty(PageObject.SERVER_IP, NetworkFunctions.localeIP().get());
 
   }
 }
