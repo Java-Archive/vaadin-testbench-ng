4e5e2678fc0c7ff5a9a483de3c5ce0c291cc6f20
Sven Ruppert <sven.ruppert@gmail.com>
2018-06-19 12:49:21 +0200

    switched to internal docker name and port (selenoid-server)

--- modules/webdriver/src/test/java/junit/org/rapidpm/vaadin/addons/testbench/BrowserDriverFunctionsTest.java
+++ modules/webdriver/src/test/java/junit/org/rapidpm/vaadin/addons/testbench/BrowserDriverFunctionsTest.java
@@ -2,7 +2,7 @@ package junit.org.rapidpm.vaadin.addons.testbench;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.rapidpm.frp.matcher.Case;
+import org.rapidpm.dependencies.core.logger.HasLogger;
 import org.rapidpm.vaadin.addons.webdriver.BrowserDriverFunctions;
 
 import java.util.Properties;
@@ -10,11 +10,12 @@ import java.util.Properties;
 import static java.lang.Boolean.TRUE;
 import static java.lang.String.valueOf;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.rapidpm.frp.matcher.Case.match;
 import static org.rapidpm.frp.matcher.Case.matchCase;
 import static org.rapidpm.frp.model.Result.failure;
 import static org.rapidpm.frp.model.Result.success;
 
-public class BrowserDriverFunctionsTest {
+public class BrowserDriverFunctionsTest implements HasLogger {
 
   @Test
   @DisplayName("test reading properties")
@@ -24,13 +25,13 @@ public class BrowserDriverFunctionsTest {
 
 
     final String unittestingTarget = valueOf(properties.get("unittesting.target")).trim();
-    Case
-        .match(
-            matchCase(() -> failure("no matching unittesting.target..")),
-            matchCase(() -> "locale".equals(unittestingTarget), () -> success(TRUE)),
-            matchCase(() -> "selenoid.rapidpm.org".equals(unittestingTarget), () -> success(TRUE)),
-            matchCase(() -> "selenoid-server".equals(unittestingTarget), () -> success(TRUE))
-        )
+    logger().info("unittestingTarget -> #" + unittestingTarget + "#");
+    match(
+        matchCase(() -> failure("no matching unittesting.target..")),
+        matchCase(() -> "locale".equals(unittestingTarget), () -> success(TRUE)),
+        matchCase(() -> "selenoid.rapidpm.org".equals(unittestingTarget), () -> success(TRUE)),
+        matchCase(() -> "selenoid-server".equals(unittestingTarget), () -> success(TRUE))
+    )
         .ifAbsent(fail("no expected property value found for unittesting.target.. " + unittestingTarget));
 
   }
